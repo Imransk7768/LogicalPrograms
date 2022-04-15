9b470fe68fed535d79a07ca4dcc5e60c785d0ce5
Imransk7768 <imransk7768@gmail.com>
2022-04-16 00:06:08 +0530

    [Imran] Added : Welcome to LogicalProgram

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>>>> Welcome to Logical Programs <<<<<<<");
+        }
+    }
+}
