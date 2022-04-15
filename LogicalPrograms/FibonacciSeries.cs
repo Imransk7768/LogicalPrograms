40d6e07093790bc928bd7a9b86b768c33e2f97e3
Imransk7768 <imransk7768@gmail.com>
2022-04-16 01:05:57 +0530

    [Imran] Added:Implementing FibonacciSeries.

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        int num1 = 0, num2 = 1, temp;
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter the Value of Series");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibbonacci Siries : ");
+            Console.Write(+num1);
+            for (int i = 1; i < num; i++)
+            {
+                temp = num1 + num2;
+                Console.Write(" " + temp);
+                num1 = num2;
+                num2 = temp;
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using LogicalPrograms;
 
-namespace LogicalPrograms
+namespace LogicalPograms
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine(">>>>>>> Welcome to Logical Programs <<<<<<<");
+            bool end = true;
+            Console.WriteLine("1.Fibonacci Series\n2.End Of The Program");
+            while (end == true)
+            {
+                Console.WriteLine("\nEnter an Option to Exicute.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                    case 2:
+                        end = false;
+                        Console.WriteLine("Program Is Ended.");
+                        break;
+                    default:
+                        Console.WriteLine("Enter the Valid Option");
+                        break;
+
+                }
+            }
         }
     }
 }
