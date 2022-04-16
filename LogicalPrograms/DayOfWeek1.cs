6e68396810739194c5ffb7f86dc9acac3deeb28e
Imransk7768 <imransk7768@gmail.com>
2022-04-16 10:02:31 +0530

    [Imran] Refactor: Day of Week

--- LogicalPrograms/DayOfWeek1.cs
+++ LogicalPrograms/DayOfWeek1.cs
@@ -14,13 +14,13 @@ namespace LogicalPrograms
             Console.Write("Enter month number between 1-12 : ");
             int month = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nEnter day of 1-7 : ");
-            int Day = Convert.ToInt32(Console.ReadLine());
+            int day = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nEnter year in YYYY format : ");
-            int Year = Convert.ToInt32(Console.ReadLine());
-            int y0 = Year - (14 - month) / 12;
+            int year = Convert.ToInt32(Console.ReadLine());
+            int y0 = year - (14 - month) / 12;
             int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
             int m0 = month + 12 * ((14 - month) / 12) - 2;
-            int d0 = (Day + x + 31 * m0 / 12) % 7;
+            int d0 = (day + x + 31 * m0 / 12) % 7;
             switch (d0)
             {
                 case 0:
